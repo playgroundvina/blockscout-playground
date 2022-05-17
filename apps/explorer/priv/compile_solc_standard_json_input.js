aa4a23b97d6fea377e014cf368b8f0aa3fce7ce5
nikitosing <nikitosing4@mail.ru>
2022-05-17 13:59:15 +0300

    Revert compile.js changes

--- apps/explorer/priv/compile_solc_standard_json_input.js
+++ apps/explorer/priv/compile_solc_standard_json_input.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 
-var inputJSONFilePath = process.argv[2]
-var compilerVersionPath = process.argv[3]
+var inputJSONFilePath = process.argv[2];
+var compilerVersionPath = process.argv[3];
 
 var solc = require('solc')
-var compilerSnapshot = require(compilerVersionPath)
-var solc = solc.setupMethods(compilerSnapshot)
+var compilerSnapshot = require(compilerVersionPath);
+var solc = solc.setupMethods(compilerSnapshot);
 
-var fs = require('fs')
-var input = fs.readFileSync(inputJSONFilePath, 'utf8')
+var fs = require('fs');
+var input = fs.readFileSync(inputJSONFilePath, 'utf8');
 
 
 const output = JSON.parse(solc.compile(input))
-console.log(JSON.stringify(output))
-process.exit()
+console.log(JSON.stringify(output));
