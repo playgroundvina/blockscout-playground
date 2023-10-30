6765ab5882ffce9a9355aa765141a4aef56dcd53
vphucnn <140361269+vphucnn@users.noreply.github.com>
2023-10-30 10:01:22 +0700

    edit content footer and button subnetwork

--- apps/block_scout_web/lib/block_scout_web/utils.ex
+++ apps/block_scout_web/lib/block_scout_web/utils.ex
@@ -1,8 +1,8 @@
 defmodule DataRender do
-  def content_footer(content \\ "PoolsScan is a Block Explorer and Analytics Platform for Pools Chain") do
+  def content_footer(content \\ "Copyright ©2023 Pools Chain. All Right Reserved.") do
     content
   end
-   def content_coppy_right(content \\ "Copyright ©2023 Pools Chain. All Right Reserved.") do
+   def content_coppy_right(content \\ "PoolsScan is a Block Explorer and Analytics Platform for Pools Chain") do
     content
   end
 end
