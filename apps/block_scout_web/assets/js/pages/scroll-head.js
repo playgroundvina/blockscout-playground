cfd9d9b597d8ae3f9c5d3b2bbb7594f6a035deb8
vphucnn <140361269+vphucnn@users.noreply.github.com>
2023-11-03 14:33:43 +0700

    edit scroll head

--- apps/block_scout_web/assets/js/pages/scroll-head.js
+++ apps/block_scout_web/assets/js/pages/scroll-head.js
@@ -1,12 +1,29 @@
-var prevScrollpos = window.pageYOffset || document.documentElement.scrollTop;
+var prevScrollpos = document.documentElement.scrollTop;
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const headAlert = document.getElementById("head-scroll");
+    const headAlertStyles = window.getComputedStyle(headAlert);
+    const h = headAlertStyles.height;
+    const main = document.getElementById("dependant-man");
+    const mainStyles = window.getComputedStyle(main);
+    const mainMarginTop = mainStyles.marginTop;
+    console.log("DOMContentLoaded")
+    console.log(h)
+    console.log(mainMarginTop)
+    console.log((parseInt(mainMarginTop) + parseInt(h) - 80) + "px")
+    main.style.marginTop = (parseInt(mainMarginTop) + parseInt(h) - 80) + "px";
+});
+
 window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset || document.documentElement.scrollTop;
-    console.log("vvv")
-    console.log(currentScrollPos)
+    var currentScrollPos = document.documentElement.scrollTop;
+    const headAlert = document.getElementById("head-scroll");
+    const headAlertStyles = window.getComputedStyle(headAlert);
+    const h = headAlertStyles.height;
     if (prevScrollpos > currentScrollPos) {
-        document.getElementById("top-navbar").style.top = "0";
+        document.getElementById("head-scroll").style.top = "0";
     } else {
-        document.getElementById("top-navbar").style.top = "-80px";
+        console.log('-' + h)
+        document.getElementById("head-scroll").style.top = '-' + h;
     }
     prevScrollpos = currentScrollPos;
 }
